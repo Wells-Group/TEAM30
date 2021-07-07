7ecd234b4ae3144802b6ad0e8a54cf40947e50ed
Jørgen Dokken <jsd55@cam.ac.uk>
2021-07-07 14:43:34 +0000

    Add visualization of both voltages

--- team30_A_phi.py
+++ team30_A_phi.py
@@ -358,8 +358,9 @@ def solve_team30(single_phase: bool, T: np.float64, omega_u: np.float64, degree:
             plt.savefig(f"{outdir}/omega_{omega_u}_{ext}.png")
 
         plt.figure()
-        plt.plot(times, VA, "-ro")
-        plt.title("Induced Voltage in Phase A")
+        plt.plot(times, VA, "-ro", label="Phase A")
+        plt.plot(times, VmA, "-ro", label="Phase -A")
+        plt.title("Induced Voltage in Phase A and -A")
         plt.grid()
         plt.legend()
         plt.savefig(f"{outdir}/voltage_{omega_u}_{ext}.png")

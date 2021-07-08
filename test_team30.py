5ff2ff4aee5405d8f21ae6223a3272e75536fe66
Jørgen Dokken <jsd55@cam.ac.uk>
2021-07-08 21:41:16 +0100

    Updates

--- test_team30.py
+++ test_team30.py
@@ -12,8 +12,9 @@ import numpy as np
 @pytest.mark.parametrize("degree", [1])
 def test_team30(single_phase, degree):
     steps = 720  # Number of steps per phase
-    rtol = 0.05  # Tolerance for relative tolerance compared to reference data
-    num_phases = 5
+    rtol = 0.05  # Tolerance for relative tolerance compared to ref data
+    atol = 1e-5
+    num_phases = 6
 
     ext = "single" if single_phase else "three"
     outdir = "test_results"
@@ -37,48 +38,61 @@ def test_team30(single_phase, degree):
     df = pandas.read_csv(f"ref_{ext}_phase.txt", delimiter=", ")
     speed = df["Speed"]
     progress = tqdm.tqdm(desc="Parametric sweep", total=len(speed))
+    cffi_options = ["-Ofast", "-march=native"]
+    jit_parameters = {"cffi_extra_compile_args": cffi_options,
+                      "cffi_libraries": ["m"]}
     for omega in speed:
         solve_team30(single_phase, num_phases, omega, degree, outdir=outdir,
-                     steps_per_phase=steps, outfile=output, progress=False, mesh_dir=outdir)
+                     steps_per_phase=steps, outfile=output, progress=False, mesh_dir=outdir,
+                     jit_parameters=jit_parameters)
         progress.update(1)
     if MPI.COMM_WORLD.rank == 0:
         # Close output file
         output.close()
 
-        # Compare results
-        df_num = pandas.read_csv(outfile, delimiter=", ")
+    # Compare results
+    df_num = pandas.read_csv(outfile, delimiter=", ")
 
-        # Torque
-        trq_ex = df["Torque"]
-        trq_vol = df_num["Torque_Arkkio"]
-        trq_surf = df_num["Torque"]
+    # Torque
+    trq_ex = df["Torque"]
+    trq_vol = df_num["Torque_Arkkio"]
+    trq_surf = df_num["Torque"]
 
-        # Voltage
-        V_ex = df["Voltage"]
-        V_num = df_num["Voltage"]
+    # Voltage
+    V_ex = df["Voltage"]
+    V_num = df_num["Voltage"]
 
-        # Loss rotor
-        L_ex = df["Rotor_loss"]
-        L_num = df_num["Rotor_loss"]
+    # Loss rotor
+    L_ex = df["Rotor_loss"]
+    L_num = df_num["Rotor_loss"]
 
-        # Loss steel
-        Ls_ex = df["Steel_loss"]
-        Ls_num = df_num["Steel_loss"]
+    # Loss steel
+    Ls_ex = df["Steel_loss"]
+    Ls_num = df_num["Steel_loss"]
 
-        print("Torque Arkkio", abs(trq_ex - trq_vol) / trq_ex)
-        print("Torque Surface", abs(trq_ex - trq_surf) / trq_ex)
+    def comp_to_print(ex, comp):
+        "Helper for printing comparison of output"
+        for i, (e, c) in enumerate(zip(ex, comp)):
+            close = np.isclose(e, c, atol=atol, rtol=rtol)
+            if not close:
+                print(f"{i}: {abs(e-c):.3e}<={atol + rtol * abs(e):.3e}")
+        print()
 
-        r_err_V = abs((V_num - V_ex) / V_ex)
-        print("Voltage", r_err_V)
+    if MPI.COMM_WORLD.rank == 0:
+        print("--------Errors-------")
+        print("Torque Arkkio")
+        comp_to_print(trq_ex, trq_vol)
+        print("Torque Surface")
+        comp_to_print(trq_ex, trq_surf)
+        print("Voltage")
+        comp_to_print(V_ex, V_num)
+        print("Rotor loss")
+        comp_to_print(L_ex, L_num)
+        print("Steel loss")
+        comp_to_print(Ls_ex, Ls_num)
 
-        r_err_loss = abs((L_num - L_ex) / L_ex)
-        print("Rotor loss", r_err_loss)
-
-        r_err_s_loss = abs((Ls_num - Ls_ex) / Ls_ex)
-        print("steel loss", r_err_s_loss)
-
-        assert np.allclose(trq_vol, trq_ex, rtol=rtol)
-        assert np.allclose(trq_surf, trq_ex, rtol=rtol)
-        assert np.allclose(V_num, V_ex, rtol=rtol)
-        assert np.allclose(L_num, L_ex, rtol=rtol)
-        assert np.allclose(Ls_num, Ls_ex, rtol=rtol)
+    assert np.allclose(trq_vol, trq_ex, rtol=rtol, atol=atol)
+    assert np.allclose(trq_surf, trq_ex, rtol=rtol, atol=atol)
+    assert np.allclose(V_num, V_ex, rtol=rtol, atol=atol)
+    assert np.allclose(L_num, L_ex, rtol=rtol, atol=atol)
+    assert np.allclose(Ls_num, Ls_ex, rtol=rtol, atol=atol)
